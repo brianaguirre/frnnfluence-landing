097e331a108c833f712ae1c7f420c5fd45bab1b0
BrianAguirre <Brian.N.Aguirre@gmail.com>
2014-04-05 11:43:56 -0400

    fixed errors on main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,10 +21,11 @@ function empty(x){
     }
   
   return empty;
-};
+}
 
 $('#phone-input').keypress(function() {
   var value = $(this).val();
   if(value.length > 9 && empty(value) === true) {
     console.log('Full');
+  }
 });
