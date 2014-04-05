0cd1191ba5327a7f63ed22b7fb714aa9aeb4575d
Helen V. Holmes <helen.v.holmes@gmail.com>
2014-04-05 10:25:07 -0400

    How to do value???

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,16 @@
 console.log('\'Allo \'Allo!');
 
-$("#phone-input").mask("(999) 999-9999"); // Formats input field properly when peeps type
+$('#phone-input').mask('(999) 999-9999'); // Formats input field properly when peeps type
+
+function empty(x){
+  for (i=0; i>=x.length(); i++){
+    if (x.val())
+  }
+}
+
+$('#phone-input').keypress(function() {
+  var value = $(this).val();
+  if(value.length > 9 && value != ' ') {
+    console.log('wassup');
+  }
+});
