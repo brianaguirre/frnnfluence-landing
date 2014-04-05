b1509c3aa47d479cb54a81ac067659224256e59d
Helen V. Holmes <helen.v.holmes@gmail.com>
2014-04-05 11:11:51 -0400

    Imma failure

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,15 +2,9 @@ console.log('\'Allo \'Allo!');
 
 $('#phone-input').mask('(999) 999-9999'); // Formats input field properly when peeps type
 
-function empty(x){
-  for (i=0; i>=x.length(); i++){
-    if (x.val())
-  }
-}
-
 $('#phone-input').keypress(function() {
   var value = $(this).val();
-  if(value.length > 9 && value != ' ') {
+  if(value.length > 9 && value.isNumeric() == 'true') {
     console.log('wassup');
   }
 });
